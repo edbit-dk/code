f9b98a251669479f47de0c089365aed70668cde2
Thomas Elvin <thomas.e.s.l@outlook.dk>
2025-04-09 22:03:20 +0200

    first

---
+++ server/code-editable.php
@@ -0,0 +1,11 @@
+<?php
+function youtube($length = 11) {
+    $characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-_';
+    $id = '';
+    for ($i = 0; $i < $length; $i++) {
+        $id .= $characters[random_int(0, strlen($characters) - 1)];
+    }
+    return $id;
+}
+
+echo youtube();
